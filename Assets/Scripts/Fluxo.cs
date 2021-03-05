c8480661ccbf076442bfaa8f190f7ff55783ccf1
Gabriel Lopes Weber <gabriellw.grb@gmail.com>
2021-03-04 21:05:56 -0300

    Interração com o botão iniciar

---
+++ Assets/Scripts/Fluxo.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Fluxo : MonoBehaviour {
+	public void CarregarFase1(){
+		SceneManager.LoadScene ("Fase1");
+	}
+}
